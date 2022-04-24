52de3e142e1b9229b2de9258a34cf3d6d08289f5
karla baltazar <baltazarkarla9@gmail.com>
2022-04-23 20:13:01 -0500

    segundo requerimiento usando TDD

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,15 +4,15 @@ const User = require("./../../app/models/User");
 describe("Unit Test  for User class", () => {
   test("Create an User object", () => {
     //se invoca el  codigo que se va a usar en la app
-    const user = new User(1, "karlaB", "karla", "Bio", "dateCreated", "lastUpdated");
+    const user = new User(1, "KarlaB", "Karla", "Bio");
 
     //aqui se validan los resultados de ese codigo
     // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
     expect(user.id).toBe(1);
-    expect(user.username).toBe("carlogilmar");
-    expect(user.name).toBe("Carlo");
+    expect(user.username).toBe("KarlaB");
+    expect(user.name).toBe("Karla");
     expect(user.bio).toBe("Bio");
-    expect(user.dateCreated).toBe("dateCreated");
-    expect(user.lastUpdated).toBe("lastUpdated");
+    expect(user.dateCreated).not.toBeUndefined();
+    expect(user.lastUpdated).not.toBeUndefined();
   });
 });
